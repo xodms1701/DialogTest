548e6602e3918a4b489dac0d3e053715c75f580d
xodms1701 <kimrrrrt123@naver.com>
2017-05-29 18:55:39 +0900

    radio button done

--- app/src/main/java/org/techtown/dialogtest/MainActivity.java
+++ app/src/main/java/org/techtown/dialogtest/MainActivity.java
@@ -23,8 +23,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("첫번째 다이얼로그");
         dialog.setIcon(R.drawable.ultraball);
-//        dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
+        /* dialog.setMessage("여기는 메시지를 쓰는 곳입니다.");
         dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                butDialog.setText(itemArr[which]);
+            }
+        });*/
+        //라디오 버튼 목록
+        dialog.setSingleChoiceItems(itemArr, 0, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 butDialog.setText(itemArr[which]);
